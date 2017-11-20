ec89bad6c66c7cf5e237ce1e65cfcb173ea5462f
cristiianefaria <cristiane.trc@gmail.com>
2017-11-20 10:39:45 -0200

    personalizando instagram carousel

--- wp-content/themes/theme-tonvalim/assets/js/instagramapi.js
+++ wp-content/themes/theme-tonvalim/assets/js/instagramapi.js
@@ -2,11 +2,42 @@ var feed = new Instafeed({
     get: 'user',
     userId: 3295626329,
     accessToken: '3295626329.a4f81a9.b13a9b826cd6490f901dca9891cfdfae',
-    template: '<div><a href="{{link}}"><img src="{{image}}" /></a></div>',
+    template: '<div><img src="{{image}}" /></div>',
     target: 'instafeed',
     after: function () {
         $('#instafeed').slick({
-            slidesToShow: 5
+            dots: true,
+            infinite: true,
+            slidesToShow: 10,
+            slidesToScroll: 1,
+            responsive: [
+                {
+                    breakpoint: 1025,
+                    settings: {
+                        slidesToShow: 6,
+                        slidesToScroll: 3,
+                        infinite: true,
+                        dots: true
+                    }
+                },
+                {
+                    breakpoint: 601,
+                    settings: {
+                        slidesToShow: 4,
+                        slidesToScroll: 4
+                    }
+                },
+                {
+                    breakpoint: 480,
+                    settings: {
+                        slidesToShow: 2,
+                        slidesToScroll: 2
+                    }
+                }
+                // You can unslick at a given breakpoint now by adding:
+                // settings: "unslick"
+                // instead of a settings object
+            ]
         });
     }
 });
--- wp-content/themes/theme-tonvalim/index.php
+++ wp-content/themes/theme-tonvalim/index.php
@@ -88,36 +88,6 @@
 
 <div id="instafeed" class="instafeed"></div>
 
-<section class="center slider-slick">
-    <div>
-      <img src="http://placehold.it/350x300?text=1">
-    </div>
-    <div>
-      <img src="http://placehold.it/350x300?text=2">
-    </div>
-    <div>
-      <img src="http://placehold.it/350x300?text=3">
-    </div>
-    <div>
-      <img src="http://placehold.it/350x300?text=4">
-    </div>
-    <div>
-      <img src="http://placehold.it/350x300?text=5">
-    </div>
-    <div>
-      <img src="http://placehold.it/350x300?text=6">
-    </div>
-    <div>
-      <img src="http://placehold.it/350x300?text=7">
-    </div>
-    <div>
-      <img src="http://placehold.it/350x300?text=8">
-    </div>
-    <div>
-      <img src="http://placehold.it/350x300?text=9">
-    </div>
-  </section>
-
 <!--  contato  -->
 <section id="contato" class="contato">
 
